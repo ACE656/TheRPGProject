6e9072a00b5f55903669ed551d24d7c4fb13aae0
Hedgy134117 <hedgy134117@gmail.com>
2019-11-26 19:55:47 -0500

    Initial Commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, -10);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(horizontal, vertical, 0f);
+    }
+}
